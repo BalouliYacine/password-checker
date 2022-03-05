713882cd044050eae983d05137c1af1e709f5d3a
BalouliYacine <cjvfyv@gmail.com>
2022-03-05 10:12:35 +0100

    add rondom password

--- src/components/2 header/header.jsx
+++ src/components/2 header/header.jsx
@@ -1,18 +1,18 @@
-import { React, useRef, useEffect } from 'react'
-import PaswordH from '../1 pasword check/password'
+import PaswordH from "../1 pasword check/password";
 
 function Header() {
-
-    return (
-        <>
-            <header>
-                <h1 >check your <span>password</span></h1>
-                <p className='HP'>If you want to test any password</p>
-                {/* <br /> */}
-                <PaswordH />
-            </header>
-        </>
-    )
+  return (
+    <>
+      <header>
+        <h1>
+          check your <span>password</span>
+        </h1>
+        <p className="HP">If you want to test any password</p>
+        {/* <br /> */}
+        <PaswordH />
+      </header>
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/6 PG/pg.jsx
+++ src/components/6 PG/pg.jsx
@@ -1,43 +1,44 @@
-import { React, useRef, useEffect, useState } from 'react'
-import { gsap } from "gsap";
-import './pg.css'
-
+import { React, useState } from "react";
+import "./pg.css";
 
 function Pg() {
+  const [gen, setgen] = useState("");
+  let [val, setval] = useState("8");
 
-    const [gen, setgen] = useState('')
-    let [val, setval] = useState('')
+  let alph1 =
+    "A a B b C c D d / § ! ? E e % F f G g H h I ù i J j K k L l M m N n O o P p Q @ & é q R r S s T t U u V v W w $ X x Y y Z z 1 2 3 4 5 6 7 8 9 0 S";
+  const Gen = () => {
+    setgen(function () {
+      let grnV = [];
+      for (let i = 0; i < val; i++) {
+        grnV.push(alph1.split(" ")[Math.floor(Math.random() * 73)]);
+      }
+      return grnV;
+    });
+  };
 
-    let alph1 = "A a B b C c D d E e F f G g H h I i J j K k L l M m N n O o P p Q q R r S s T t U u V v W w X x Y y Z z 1 2 3 4 5 6 7 8 9 0"
-    let alph2 = "+ ( | ) % m N _ O o P p Q q R r S s & ! U u V v [ ] X x Y y Z z - / B b C c D d # é F f G g H h I i J j 3 1 4 8 7 1 2 9 0 5"
-    let newpwd = []
+  // const valch = (e) => {
+  //   setval(e.target.value);
+  // };
+  const Ran = (e) => {
+    setval(e.target.value);
+    Gen();
+  };
 
-    const Gen = () => {
-        let pwd = [val.split(' ')];
-
-        for (let i = 0; i < pwd.length; i++) {
-            let pwd2 = pwd[i][0].split('')
-            for (let g = 0; g < pwd2.length; g++) {
-                newpwd.push(alph2[alph1.indexOf(pwd2[g])])
-            }
-        }
-        setgen(newpwd.join(''))
-    }
-
-    const valch = (e) => {
-        setval(e.target.value)
-    }
-
-    return (
-        <div className='pg'>
-            <h1 >Password Generator (soon)</h1>
-            <br />
-            <input type="text" value={val} onChange={valch} />
-            <button onClick={Gen}> Generate </button>
-            <br /><br />
-            <p> {gen} </p>
-        </div>
-    )
+  return (
+    <div className="pg">
+      <h1>Password Generator (soon)</h1>
+      <br />
+      <input type="range" onChange={Ran} min="8" max="30" defaultValue={8} />
+      <span>{val} </span>
+      <br />
+      {/* <input type="text" value={val} onChange={valch} /> */}
+      <button onClick={Gen}> Generate </button>
+      <br />
+      <br />
+      <p> {gen} </p>
+    </div>
+  );
 }
 
-export default Pg
+export default Pg;
